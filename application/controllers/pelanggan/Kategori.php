244771a2711af90d6b82ae605e3346ceb96e324d
arinifir <55473280+arinifir@users.noreply.github.com>
2020-12-22 05:42:53 +0700

    Detail Produk

--- application/controllers/pelanggan/Kategori.php
+++ application/controllers/pelanggan/Kategori.php
@@ -7,7 +7,8 @@ class Kategori extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("M_produk");
+        $this->load->model("M_produk","produk");
+        $this->load->model("M_admin","admin");
         $this->load->model("M_pelanggan");
         //load model admin
         $this->load->helper('auth_helper');
@@ -22,9 +23,9 @@ class Kategori extends CI_Controller
         $harga_min = $this->input->get('min');
         $harga_max = $this->input->get('max');
         if ($kategori || $harga_min || $harga_max) {
-            $data['produk'] = $this->M_produk->getByFilter($harga_min, $harga_max, $kategori)->result();
+            $data['produk'] = $this->produk->getByFilter($harga_min, $harga_max, $kategori)->result();
         } else {
-            $data['produk'] = $this->M_produk->getAll();
+            $data['produk'] = $this->produk->getAll();
         }
         // var_dump($data['produk']);die;
         $data['produk'] = $this->M_pelanggan->getAll('tb_produk');
