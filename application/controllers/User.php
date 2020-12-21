6ce694337809f92b8650ed5b1354d8e733d32ac8
Fahrul Irsyad Fianto <variationfianto@gmail.com>
2020-12-22 01:47:25 +0700

    salah function

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,7 +7,7 @@ class User extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('M_produk');
+        $this->load->model("M_produk");
         //load model admin
         $this->load->helper('auth_helper');
         $this->load->library('user_agent');
@@ -17,13 +17,7 @@ class User extends CI_Controller
 
     public function index()
     {
-        $kategori = $this->input->get('ktg');
-        $harga_min = $this->input->get('harga_min');
-        $harga_max = $this->input->get('harga_max');
-        if ($kategori || $h) {
-            # code...
-        }
-        $data['produk'] = $this->M_produk->getAll();
+
         $data['judul'] = "ALT Printing - Home";
         $this->load->view('user/header', $data);
         $this->load->view('user/topbar');
